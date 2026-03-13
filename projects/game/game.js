7806814f33d4541744bfacbf30718d525e783858
ColeHarmaty <ch15248@email.vccs.edu>
2026-03-13 09:18:52 -0400

    got it worked on local file

--- projects/game/game.js
+++ projects/game/game.js
@@ -36,16 +36,16 @@ if (time == 12) {
 //Make one function for each location
 function Cafeteria() {
     clear();
-    print("You are surrounded by tentacle monsters, and time is frozen." +"\nYou can either run, or stop trying to keep the power in your head, well in \nyour head. You can sense that it will doing something, but you're not sure\nwhat");
-    print("\n What do you want to do? Say one of these choices:" + "\n\run"+"\nlet the power out");
+    print("\n\tYou are surrounded by tentacle monsters, and time is frozen." +"\nYou can either run, or stop trying to keep the power in your head, well in \nyour head. You can sense that it will doing something, but you're not sure\nwhat");
+    print("\nWhat do you want to do? Say one of these choices:" + "\n\trun"+"\n\tlet the power out");
     
     function processInput(input){
         if (input.toLowerCase() === "run") {
 		time =+ 5;
-		print("\nYou've never did any kind of sports, but pherhaps you should've\n. You sprint for the door with the speed of a cheetah, and it shows.\n Unfortunatly, they are faster than you are. You are about to be torn\napart by the tenactles when the chimps burst through the door, there faces\nshowing what, on a human, would be a smile. On a chimpaneze, it means they\nare pissed. They leap on to the monsters with a savage fury, tearing them\napart in a savage display of guts and fury. They pay for their injuries, but\nall the chimps survive. They will need medical treatement. You don't want\nto find out what is in those claws the hard way. You know you can find \nwhat you need in the Chimp Zone. You file out into the hallway with the chimps\n and firmly lock the door behind you. When you look behind you, the chimps are gone.");		
-        } else {
+		print("\n\tYou've never did any kind of sports, but pherhaps you should've\n. You sprint for the door with the speed of a cheetah, and it shows.\n Unfortunatly, they are faster than you are. You are about to be torn\napart by the tenactles when the chimps burst through the door, there faces\nshowing what, on a human, would be a smile. On a chimpaneze, it means they\nare pissed. They leap on to the monsters with a savage fury, tearing them\napart in a savage display of guts and fury. They pay for their injuries, but\nall the chimps survive. They will need medical treatement. You don't want\nto find out what is in those claws the hard way. You know you can find \nwhat you need in the Chimp Zone. You file out into the hallway with the chimps\n and firmly lock the door behind you. When you look behind you, the chimps are gone.");		
+        } else if (input.toLowerCase() === "let the power out") {
             Magic =+ 1;
-		print("You stop holding the power in. It shoots out of you in a sphere \nshaped ripple of something. When the ripple passes over the tentacle monsters,\nthey seem to crumble to dust. You stumble out the door, your headaching, and something urging you to check on the chimps.");
+		print("\n\tYou stop holding the power in. It shoots out of you in a sphere \nshaped ripple of something. When the ripple passes over the tentacle monsters,\nthey seem to crumble to dust. You stumble out the door, your headaching, and something urging you to check on the chimps.");
         }
     }
     waitThenCall(Hallway());
@@ -54,12 +54,12 @@ function Cafeteria() {
 function Hallway() {
     clear();
 	timecheck();
-    print("\nYou are in the Hallway, with the door to the cafeteria shut and sealed\n firmly behind you. From here, you can see the signs saying ChimpZone,\nCafeteria, and Reactor.");
-    print("\nWhat do you want to do next? Say one of these choices:" + "Stay Here"+"\n\tGo To Cafeteria"+"\n\tGo to ChimpZone"+"Go to Reactor");
+    print("\n\tYou are in the Hallway, with the door to the cafeteria shut and sealed\n firmly behind you. From here, you can see the signs saying ChimpZone,\nCafeteria, and Reactor.");
+    print("\nWhat do you want to do next? Say one of these choices:" + "\n\tStay Here"+"\n\tGo To Cafeteria"+"\n\tGo to ChimpZone"+"\n\tGo to Reactor");
     
     function processInput(input){
         if (input.toLowerCase() === "Cafeteria") {
-            print("You try to step into the Cafeteria. But as you move to open the doors, you flash back to the moment everyone you cared for, everyone but your distant aquantances, died. You jump back from the door like you've been burned. You won't go back there.");
+            print("\n\tYou try to step into the Cafeteria. But as you move to open the doors,\nyou flash back to the moment everyone you cared for, everyone but your distant\naquantances, died. You jump back from the door like you've been burned. You\nwon't go back there.");
 time =+ 5;
 waitThenCall(Hallway());
         }
@@ -68,8 +68,7 @@ waitThenCall(Hallway());
 			   ChimpZone();
 		   }
 			   else if (input.toLowerCase() === "reactor"){
-			   {
-				   Reactor();
+			    Reactor();
 			   }
 			   else {
 				   StayHere();
@@ -79,20 +78,19 @@ waitThenCall(Hallway());
     }
     waitForInput(processInput);
 }
-}
 
 function ChimpZone() {
 clear();
 timecheck();                                                            
-    print("\nYou are in the ChimpZone! The Chimps live here and most of the \ntime, so do you. You are the one who takes care of the chimps, after all.\nEverything you could want to care for an injured chimp can be found here. Al\nthe chimps' other supplies are also here, from toys to toilets. There is a booth\nacross the room where the chimps can pilot the ship from in the case of\nhumans turning out to be unable to navigate the Void. It requires permission codes\nto be activated, and you don't have all the codes nessecary to do so.\n\n\tUnfortunalty, even though the helm is almost certainly logged in,it still requires\nthose codes to activate the booth down here. You don't have enough time to jury-rig\na solution. Otherwise, you'd let the chimps do the job they were so good at,\nand get you out of here. The human helm is also significantly more complex than\nthe one down here, as it does other functions besides controlling the ship in\nthe Void. Otherwise, you'd be bringing a chimp with you to the helm.");
+    print("\n\tYou are in the ChimpZone! The Chimps live here and most of the \ntime, so do you. You are the one who takes care of the chimps, after all.\nEverything you could want to care for an injured chimp can be found here. Al\nthe chimps' other supplies are also here, from toys to toilets. There is a booth\nacross the room where the chimps can pilot the ship from in the case of\nhumans turning out to be unable to navigate the Void. It requires permission codes\nto be activated, and you don't have all the codes nessecary to do so.\n\n\tUnfortunalty, even though the helm is almost certainly logged in,it still requires\nthose codes to activate the booth down here. You don't have enough time to jury-rig\na solution. Otherwise, you'd let the chimps do the job they were so good at,\nand get you out of here. The human helm is also significantly more complex than\nthe one down here, as it does other functions besides controlling the ship in\nthe Void. Otherwise, you'd be bringing a chimp with you to the helm.");
 	if (ChimpsSaved === 0){
 		print("\nThere are four injured chimps in cages.");
 	}
 	if(Magic > 0){
-		print("How did they get injured? They never left there cages.");
+		print("\nHow did they get injured? They never left there cages.");
 	}
 	else{
-print("I didn't hallucinate the chimps saving me after all. They have the same\ninjuries that I saw them take in the cafeteria. But then, how did they get here,");	
+print("\n\tI didn't hallucinate the chimps saving me after all. They have the same\ninjuries that I saw them take in the cafeteria. But then, how did they get here,");	
 	}
 	print("\nWhat do you want to do? Say one of these choices:" + "\nStay Here"+ "\nGo To Hallway");
 	if (ChimpsSaved === 0){
@@ -101,7 +99,7 @@ print("I didn't hallucinate the chimps saving me after all. They have the same\n
 function processInput(input){
 	if (input.toLowerCase() === "help the chimps"){
 ChimpsSaved =+ 1;
-print("\nYou spend a few minutes cleaning out the chimps' wounds. It's soothing, and, based\non the fact that the flesh was starting to change color, you don't want to know\nwhat would've happened if you forgot to check on the chimps.");
+print("\n\tYou spend a few minutes cleaning out the chimps' wounds. It's soothing, and, based\non the fact that the flesh was starting to change color, you don't want to know\nwhat would've happened if you forgot to check on the chimps.");
 	ChimpZone();
 	}
 	else if (input.toLowerCase() === "hallway"){
@@ -116,7 +114,7 @@ print("\nYou spend a few minutes cleaning out the chimps' wounds. It's soothing,
 function Reactor(){
 	clear();
 	timecheck();
-	print("\n\tYou are in the Reactor! This is where the ship's power is generated in\na state of the art fusion reactor, andwhere the ship's Void drive, currently powered down is. At least,\nthat's what you've been told. For all you know, it could be a really expensive\npeice of modern art. There should be people here, or at least what used to\nbe people, but there aren't any more. The only thing here that you haven't seen 20\ntimes before makes you feel like you got stabbed in the chest the instant you see\nit.\n\n\tA man in a security officers uniform, his body halfway transformed into one of\nthose things, half man and half monstrosity, with his left arm halfway transformed into\na tentacle. Given the gun in his hand and the way his skull looks like it exploded,\nyou now know roughly whathappened here.\nIn a daze, you do something very stupid, something that your biology teachers would\nyell at you for so much as considering, you touch the corpse. You recognize it in a heartbeat.\n\n\tSergeant Alexander Ivanov, the head of the security forces on this ship. People\nget rowdy in space, and the most he usaully has to deal with is breaking up a fistfight.\nYou feel the urge to salute to him, knowing what must have happened. You indulge\nit. It's nowhere near what he deserves, what any of the 299 ghosts that died on this\nship deserve, but something is better than nothing. And you will be thatsomething.\n "I'll see you on the other side in about 5 minutes if this doesn't work,"\nYou tell him, having seen the sign leading to the helm.\n\n\tWhat do you want to do? Say one of these choices:\nStay Here\nGo back to the Hallway\nGo to the Helm");
+	print("\n\tYou are in the Reactor! This is where the ship's power is generated in\na state of the art fusion reactor, and where the ship's Void drive, currently powered down is. At least,\nthat's what you've been told. For all you know, it could be a really expensive\npeice of modern art. There should be people here, or at least what used to\nbe people, but there aren't any more. The only thing here that you haven't seen 20\ntimes before makes you feel like you got stabbed in the chest the instant you see\nit.\n\n\tA man in a security officers uniform, his body halfway transformed into one of\nthose things, half man and half monstrosity, with his left arm halfway transformed into\na tentacle. Given the gun in his hand and the way his skull looks like it exploded,\nyou now know roughly whathappened here.\nIn a daze, you do something very stupid, something that your biology teachers would\nyell at you for so much as considering, you touch the corpse. You recognize it in a heartbeat.\n\n\tSergeant Alexander Ivanov, the head of the security forces on this ship. People\nget rowdy in space, and the most he usaully has to deal with is breaking up a fistfight.\nYou feel the urge to salute to him, knowing what must have happened. You indulge\nit. It's nowhere near what he deserves, what any of the 299 ghosts that died on this\nship deserve, but something is better than nothing. And you will be thatsomething.\n \"I'll see you on the other side in about 5 minutes if this doesn't work,\"\nYou tell him, having seen the sign leading to the helm.\n\n\tWhat do you want to do? Say one of these choices:\nStay Here\nGo back to the Hallway\nGo to the Helm");
 function processInput(input){
 	if (input.toLowerCase === "go back to the hallway"){
 		Hallway();
@@ -133,7 +131,7 @@ StayHere();
 function Helm(){                                                            
         clear();
 	timecheck();
-print("\nThe Helm. Where the ship is piloted from. Normally, you'd never be here.\nIn a world where the pilot wasn't dead, you wouldn't. The helm is blinking just\n as you predicted. It's still logged in. It looks familiar to you, just as you\nhoped. You've never done this before, never trained for it, but you've helped\nthe chimps do so enough to instantly identify most of the controls, and seen\nenough of the sims the chimps trained in to have an idea of what you need to\ndo to fly the ship."+"\n\nWhat do you want to do? Say one of these choices:" + "\nStay Here" + "\nGo back to the Reactor" + "fly the Ship");
+print("\n\tThe Helm. Where the ship is piloted from. Normally, you'd never be here.\nIn a world where the pilot wasn't dead, you wouldn't. The helm is blinking just\n as you predicted. It's still logged in. It looks familiar to you, just as you\nhoped. You've never done this before, never trained for it, but you've helped\nthe chimps do so enough to instantly identify most of the controls, and seen\nenough of the sims the chimps trained in to have an idea of what you need to\ndo to fly the ship.\n\nWhat do you want to do? Say one of these choices:\nStay Here\nGo back to the Reactor\nfly the Ship");
 function processInput(input){
 	if (input.toLowerCase === "go back to the reactor"){
                 Reactor();

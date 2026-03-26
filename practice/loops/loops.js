53ff4f8e3dd55973922b558935a2ed1f7e71f731
ColeHarmaty <ch15248@email.vccs.edu>
2026-03-26 11:34:06 -0400

    my code for canvas 1, just can't get it too show up in canvas

---
+++ practice/loops/loops.js
@@ -0,0 +1,27 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+function Run1() {
+for (let y = 0; y <= 200; y = y + 100) {
+for (let x = 0; x <= 700; x = x + 100) {
+ctx.fillRect(x, y, 10, 70);
+}
+}
+}
+
+//fill in code for canvas1 here
+
+canvas = document.getElementById("canvas2");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas2 here
+
+canvas = document.getElementById("canvas3");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas3 here
+
+canvas = document.getElementById("canvas4");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas4 here
+
